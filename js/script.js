a7e54c055c432de1ef6be8eac79c8573a9324a16
lacclaude <38310130+lacclaude@users.noreply.github.com>
2019-07-03 02:10:34 +0800

    no message

---
+++ js/script.js
@@ -0,0 +1,7 @@
+$(function() {
+
+  // Slider
+  $('#coin-slider').coinslider({width:960,height:540,opacity:1});
+});	
+
+Cufon.replace('.menu_nav ul li a, p.infopost a.com span, h1, h2, h3, h4, h5, h6', { hover: true });
