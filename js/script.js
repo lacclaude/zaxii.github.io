8e65f8e535dc33276e012562e693ec4a8d6f5714
lacclaude <38310130+lacclaude@users.noreply.github.com>
2019-07-03 21:05:39 +0800

    no message

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ $(function()
     $('#coin-slider').coinslider({width:960,height:540,opacity:1});
 });	
 
-Cufon.replace('.menu_nav ul li a, p.infopost a.com span, h1, h2, h3, h4, h5, h6', { hover: true });
-
 function saveStorage(id,id2) 
 {
   var data = document.getElementById(id).value; //取值
